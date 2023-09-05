23908f301c06f3dd8b10de41c58cd7e106e7698d
doodlum <15017472+doodlum@users.noreply.github.com>
2023-09-05 15:02:21 +0100

    Initial commit

---
+++ Plugin/src/main.cpp
@@ -0,0 +1,58 @@
+#include "SFSE/Stub.h"
+
+DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
+	SFSE::PluginVersionData data{};
+
+	data.PluginVersion(Plugin::Version);
+	data.PluginName(Plugin::NAME);
+	data.AuthorName(Plugin::AUTHOR);
+	data.UsesSigScanning(true);
+	//data.UsesAddressLibrary(true);
+	data.HasNoStructUse(true);
+	//data.IsLayoutDependent(true);
+	data.CompatibleVersions({ RUNTIME_VERSION_1_7_23 });
+
+	return data;
+}();
+
+namespace
+{
+	void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept
+	{
+		switch (a_msg->type) {
+		case SFSE::MessagingInterface::kPostLoad:
+			{
+				break;
+			}
+		default:
+			break;
+		}
+	}
+}
+
+/**
+// for preload plugins
+void SFSEPlugin_Preload(SFSE::LoadInterface* a_sfse);
+/**/
+
+DLLEXPORT bool SFSEAPI SFSEPlugin_Load(SFSEInterface* a_sfse)
+{
+#ifndef NDEBUG
+	while (!IsDebuggerPresent()) {
+		Sleep(100);
+	}
+#endif
+
+	SFSE::Init(a_sfse);
+
+	DKUtil::Logger::Init(Plugin::NAME, std::to_string(Plugin::Version));
+
+	INFO("{} v{} loaded", Plugin::NAME, Plugin::Version);
+
+	// do stuff
+	SFSE::AllocTrampoline(1 << 10);
+
+	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
+
+	return true;
+}
