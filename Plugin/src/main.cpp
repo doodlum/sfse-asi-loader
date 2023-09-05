2efe3c90573bb36858ca0e00b22f9d036f9f5993
Tim <15017472+doodlum@users.noreply.github.com>
2023-09-05 15:29:05 +0100

    Initial attempt

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -1,4 +1,39 @@
 #include "SFSE/Stub.h"
+#include <codecvt>
+
+//MIT License
+//
+//	Copyright(c) 2018 ThirteenAG
+//
+//	Permission is hereby granted,
+//	free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+//	The above copyright notice and this permission notice shall be included in all copies
+//	or
+//	substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS",
+//	WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//	DAMAGES OR OTHER
+//	LIABILITY,
+//	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+
+HMODULE hm = nullptr;
+
+BOOL APIENTRY DllMain(HMODULE a_hModule, DWORD fdwReason, LPVOID)
+{
+	switch (fdwReason) {
+	case DLL_PROCESS_ATTACH:
+		hm = a_hModule;
+		break;
+	}
+
+	return TRUE;
+}
 
 DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
 	SFSE::PluginVersionData data{};
@@ -15,27 +50,141 @@ DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
 	return data;
 }();
 
-namespace
+std::wstring GetCurrentDirectoryW()
 {
-	void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept
-	{
-		switch (a_msg->type) {
-		case SFSE::MessagingInterface::kPostLoad:
-			{
-				break;
-			}
-		default:
-			break;
+	static constexpr auto INITIAL_BUFFER_SIZE = MAX_PATH;
+	static constexpr auto MAX_ITERATIONS = 7;
+	std::wstring ret;
+	auto bufferSize = INITIAL_BUFFER_SIZE;
+	for (size_t iterations = 0; iterations < MAX_ITERATIONS; ++iterations) {
+		ret.resize(bufferSize);
+		auto charsReturned = GetCurrentDirectoryW(bufferSize, ret.data());
+		if (charsReturned < ret.length()) {
+			ret.resize(charsReturned);
+			return ret;
+		} else {
+			bufferSize *= 2;
 		}
+	}
+	return L"";
+}
+
+HMODULE LoadLibraryW(const std::wstring& lpLibFileName)
+{
+	return LoadLibraryW(lpLibFileName.c_str());
+}
+
+std::wstring GetModuleFileNameW(HMODULE hModule)
+{
+	static constexpr auto INITIAL_BUFFER_SIZE = MAX_PATH;
+	static constexpr auto MAX_ITERATIONS = 7;
+	std::wstring ret;
+	auto bufferSize = INITIAL_BUFFER_SIZE;
+	for (size_t iterations = 0; iterations < MAX_ITERATIONS; ++iterations) {
+		ret.resize(bufferSize);
+		auto charsReturned = GetModuleFileNameW(hModule, &ret[0], bufferSize);
+		if (charsReturned < ret.length()) {
+			ret.resize(charsReturned);
+			return ret;
+		} else {
+			bufferSize *= 2;
+		}
+	}
+	return L"";
+}
+
+std::string ws2s(const std::wstring& wstr)
+{
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+	return converterX.to_bytes(wstr);
+}
+
+void FindFiles(WIN32_FIND_DATAW* fd)
+{
+	auto dir = GetCurrentDirectoryW();
+
+	HANDLE asiFile = FindFirstFileW(L"*.asi", fd);
+	if (asiFile != INVALID_HANDLE_VALUE) {
+		do {
+			if (!(fd->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+				auto pos = wcslen(fd->cFileName);
+
+				if (fd->cFileName[pos - 4] == '.' &&
+					(fd->cFileName[pos - 3] == 'a' || fd->cFileName[pos - 3] == 'A') &&
+					(fd->cFileName[pos - 2] == 's' || fd->cFileName[pos - 2] == 'S') &&
+					(fd->cFileName[pos - 1] == 'i' || fd->cFileName[pos - 1] == 'I')) {
+					auto path = dir + L'\\' + fd->cFileName;
+
+					if (GetModuleHandle(path.c_str()) == NULL) {
+						INFO("Loading {}", ws2s(path));
+
+						auto h = LoadLibraryW(path);
+						SetCurrentDirectoryW(dir.c_str());  // In case asi switched it
+
+						if (h == NULL) {
+							auto e = GetLastError();
+							ERROR("Failed to load plugin {} : Error {}", ws2s(path), e);
+							if (e != ERROR_DLL_INIT_FAILED && e != ERROR_BAD_EXE_FORMAT)  // In case dllmain returns false or IMAGE_MACHINE is not compatible
+							{
+								std::wstring msg = L"Unable to load " + std::wstring(fd->cFileName) + L". Error: " + std::to_wstring(e);
+								MessageBoxW(0, msg.c_str(), L"ASI Loader", MB_ICONERROR);
+							}
+						} else {
+							INFO("Loaded plugin");
+
+							auto procedure = (void (*)())GetProcAddress(h, "InitializeASI");
+
+							if (procedure != NULL) {
+								INFO("InitializeASI found, calling");
+								procedure();
+							} else {
+								ERROR("InitializeASI not found");
+							}
+						}
+					}
+				}
+			}
+		} while (FindNextFileW(asiFile, fd));
+		FindClose(asiFile);
 	}
 }
 
-/**
-// for preload plugins
-void SFSEPlugin_Preload(SFSE::LoadInterface* a_sfse);
-/**/
+void LoadPlugins()
+{
+	auto oldDir = GetCurrentDirectoryW();  // Store the current directory
 
-DLLEXPORT bool SFSEAPI SFSEPlugin_Load(SFSEInterface* a_sfse)
+	auto szSelfPath = GetModuleFileNameW(hm).substr(0, GetModuleFileNameW(hm).find_last_of(L"/\\") + 1);
+	SetCurrentDirectoryW(szSelfPath.c_str());
+
+	WIN32_FIND_DATAW fd;
+
+	INFO("Loading plugins from {}", ws2s(szSelfPath));
+	FindFiles(&fd);
+
+	SetCurrentDirectoryW(szSelfPath.c_str());
+
+	if (SetCurrentDirectoryW(L"Scripts\\")) {
+		INFO("Loading plugins from {}", ws2s(szSelfPath));
+		FindFiles(&fd);
+	} else {
+		INFO("Not loading plugins from {} : Error {}", ws2s(szSelfPath), GetLastError());
+	}
+
+	SetCurrentDirectoryW(szSelfPath.c_str());
+
+	if (SetCurrentDirectoryW(L"Plugins\\")) {
+		INFO("Loading plugins from {}", ws2s(szSelfPath));
+		FindFiles(&fd);
+	} else {
+		INFO("Not loading plugins from {} : Error {}", ws2s(szSelfPath), GetLastError());
+	}
+
+	SetCurrentDirectoryW(oldDir.c_str());  // Reset the current directory
+}
+
+DLLEXPORT bool SFSEAPI SFSEPlugin_Preload(SFSEInterface* a_sfse)
 {
 #ifndef NDEBUG
 	while (!IsDebuggerPresent()) {
@@ -49,10 +198,12 @@ DLLEXPORT bool SFSEAPI SFSEPlugin_Load(SFSEInterface* a_sfse)
 
 	INFO("{} v{} loaded", Plugin::NAME, Plugin::Version);
 
-	// do stuff
-	SFSE::AllocTrampoline(1 << 10);
+	LoadPlugins();
 
-	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
+	return true;
+}
 
+DLLEXPORT bool SFSEAPI SFSEPlugin_Load(SFSEInterface* a_sfse)
+{
 	return true;
 }
