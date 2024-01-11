ba1c71b028618d8213ada64b1db10d1964619be8
Ian E <iane@iane.dev>
2024-01-11 00:57:54 -0800

    Variable Fix

--- .ghaction-scripts/build-test.ps1
+++ .ghaction-scripts/build-test.ps1
@@ -1,3 +1,3 @@
-Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -Confirm:$False | Out-Null
-& cmake -B $PSScriptRoot/build -S $PSScriptRoot/Plugin --preset=REL ---DCMAKE_TOOLCHAIN_FILE=${{ github.workspace }}/extern/vcpkg/scripts/buildsystems/vcpkg.cmake
-& cmake --build $PSScriptRoot/build --config Release
+Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction SilentlyContinue -Confirm:$False | Out-Null
+& cmake -B $PSScriptRoot/build -S $PSScriptRoot/Plugin --preset=REL -DCMAKE_TOOLCHAIN_FILE="${env:GITHUB_WORKSPACE}/extern/vcpkg/scripts/buildsystems/vcpkg.cmake"
+& cmake --build $PSScriptRoot/build --config Release -v
