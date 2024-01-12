4dff0c2178e2ba3579c632ba24d31841d37b6fdf
Ian E <iane@iane.dev>
2024-01-12 02:47:18 -0800

    Update build system and dependencies
    Updated with lots of tiny fixes and will now actually build from source.

--- build-release.ps1
+++ build-release.ps1
@@ -1,3 +1,3 @@
-Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -Confirm:$False | Out-Null
-& cmake -B $PSScriptRoot/build -S $PSScriptRoot/Plugin --preset=REL
+ReRemove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -Confirm:$False | Out-Null
+& cmake -B $PSScriptRoot/build -S $PSScriptRoot/Plugin --preset=build-release-msvc-msvc -DCMAKE_TOOLCHAIN_FILE="$PSScriptRoot/extern/vcpkg/scripts/buildsystems/vcpkg.cmake"
 & cmake --build $PSScriptRoot/build --config Release
