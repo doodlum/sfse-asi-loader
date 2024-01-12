57bc2985e93301acac9e645b93ed9e21a6e92b6f
clang-format <IanE-Official@users.noreply.github.com>
2024-01-12 10:47:51 +0000

    chore: style formatting

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -41,8 +41,8 @@ std::wstring GetCurrentDirectoryW()
 {
 	static constexpr auto INITIAL_BUFFER_SIZE = MAX_PATH;
 	static constexpr auto MAX_ITERATIONS = 7;
-	std::wstring ret;
-	auto bufferSize = INITIAL_BUFFER_SIZE;
+	std::wstring          ret;
+	auto                  bufferSize = INITIAL_BUFFER_SIZE;
 	for (size_t iterations = 0; iterations < MAX_ITERATIONS; ++iterations) {
 		ret.resize(bufferSize);
 		auto charsReturned = GetCurrentDirectoryW(bufferSize, ret.data());
@@ -65,8 +65,8 @@ std::wstring GetModuleFileNameW(HMODULE hModule)
 {
 	static constexpr auto INITIAL_BUFFER_SIZE = MAX_PATH;
 	static constexpr auto MAX_ITERATIONS = 7;
-	std::wstring ret;
-	auto bufferSize = INITIAL_BUFFER_SIZE;
+	std::wstring          ret;
+	auto                  bufferSize = INITIAL_BUFFER_SIZE;
 	for (size_t iterations = 0; iterations < MAX_ITERATIONS; ++iterations) {
 		ret.resize(bufferSize);
 		auto charsReturned = GetModuleFileNameW(hModule, &ret[0], bufferSize);
